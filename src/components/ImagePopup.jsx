2989715420229e84a4642383474990a325030b02
Aliya Matygulina <aliya_kh@mail.ru>
2023-06-13 16:16:29 +0500

    10спринт 2 попытка

--- src/components/ImagePopup.jsx
+++ src/components/ImagePopup.jsx
@@ -4,7 +4,7 @@ export default function ImagePopup({card, onClose}) {
   return (
     <div className={`popup popup_type_open-image ${card ? 'popup_opened' : ''}`}>
       <div className='popup__image-conteiner'>
-        <img className="popup__image" src={card ? card.link : '#'} alt="увеличенное изображение"/>
+        <img className="popup__image" src={card ? card.link : '#'} alt={card ? card.name : ''}/>
         <h2 className="popup__title-card">{card ? card.name : ''}</h2>
         <button className="popup__close-button" type="button" aria-label="закрыть" onClick={onClose}></button>
       </div>
